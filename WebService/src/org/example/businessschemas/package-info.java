79306c4ce7a7ee2c9634abc3731cdd9862a5c0e7
Gregor Majcen <majcn.m@gmail.com>
2013-04-20 12:24:00 +0200

    delete webService

--- WebService/src/org/example/businessschemas/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/businessSchemas", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.example.businessschemas;
